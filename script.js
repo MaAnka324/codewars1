536b9d0e749244e0cec59148eb52c9349e2d71cf
MaAnka324 <115251389+MaAnka324@users.noreply.github.com>
2023-01-03 12:53:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 // Преобразование числа в строку!
-// function numberToString(num) {
+function numberToString(num) {
 //     // Return a string of the number here!
-//   return String(num)
-//   }
+  return String(num)
+  }
 
 
-//   function paperwork(n, m) {
-//     if(n > 0){
-//       if( m > 0){
-//         return n * m;
-//       }
-//       else{
-//         return 0;
-//       }
-//       }
-//      else{
-//         return 0;
-//     }}
-// paperwork(5, 5);
+  function paperwork(n, m) {
+    if(n > 0){
+      if( m > 0){
+        return n * m;
+      }
+      else{
+        return 0;
+      }
+      }
+     else{
+        return 0;
+    }}
+paperwork(5, 5);
 
 
 
 /////////////////////////////1
-// function invert(array) {
-//     for (let i = 0; i < array.length; i++) {
-//           if(array[i] === "0") {
-//             array[i] = array[i] * (1);
-//           }
-//           array[i] = array[i] * (-1);
-//        }
-//     return array ;
-//  }
+function invert(array) {
+    for (let i = 0; i < array.length; i++) {
+          if(array[i] === "0") {
+            array[i] = array[i] * (1);
+          }
+          array[i] = array[i] * (-1);
+       }
+    return array ;
+ }
 
 ///////////////////////////////////////2
  function invert(array) {
@@ -39,3 +39,15 @@
     }
      return array;
   }
+
+
+////////////////////////////
+function saleHotdogs(n){
+  if(n < 5){
+    return n * 100;
+  }
+  else if(n >= 5 && n < 10){
+    return n * 95
+  }
+  else if(n >= 10) return n * 90
+}
